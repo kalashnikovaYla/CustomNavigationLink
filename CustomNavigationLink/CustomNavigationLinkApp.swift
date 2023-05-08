c4e1a909881ec2ea78a402883ef41d9853031f76
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-08 23:00:52 +0500

    Initial Commit

---
+++ CustomNavigationLink/CustomNavigationLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationLinkApp.swift
+//  CustomNavigationLink
+//
+//  Created by sss on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
